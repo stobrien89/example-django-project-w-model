882948226e8f7880f3c08e7cab8071beabd00423
stobrien89 <stobrien@crimson.ua.edu>
2021-09-25 09:12:51 -0700

    add routes to app

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+# For sending JSON responses
+from django.http import JsonResponse
+# To serialize objects into json strings
+from django.core.serializers import serialize
+# To convert json strings into dictionaries
+import json
 
-# Create your views here.
+from .models import Turtle
+# View class
+from django.views import View
+# import helper function
+from .helpers import GetBody
+
+# class for model routes
+
+
+class TurtleView(view):
+    # Index route
+    def get(self, request):
+        # get all
+        all = Turtle.objects.all()
+        # serialize object into json string
+        serialized = serialize("json", all)
+        # convert json string into a dictionary
+        final_data = json.loads(serialized)
+        # send json response
+        return JsonResponse(final_data, safe=False)
+
+    # Create route
+    def post(self, request):
+        # get data from request body
+        body = GetBody(request)
+        print(body)
+        # Create new db item
+        turtle = Turtle.objects.create(name=body["name"], age=body["age"])
+        # Convert new item into json string, then a dict
+        final_data = json.loads(serialize("json", [turtle]))
+        # Send json response
+        return JsonResponse(final_data, safe=False)
