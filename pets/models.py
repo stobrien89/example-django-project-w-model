0d62071a74b48e4b09e5d41eaa78b423995d7db2
stobrien89 <stobrien@crimson.ua.edu>
2021-09-25 08:38:47 -0700

    add first model to pets app

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,11 @@
+# The Model class we will inherit from
 from django.db import models
 
-# Create your models here.
+# new model class
+
+
+class Turtle(models.Model):
+    # define a string filed of max 100 characters
+    name = models.CharField(max_length=100)
+    # define an age that is an integer
+    age = models.IntegerField
